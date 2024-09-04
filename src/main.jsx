905f628670d32e44d6d532ec1c26f03ca63b6a47
Rajvardhan135 <email_address>
2024-09-04 11:41:21 +0530

    Add @vercel/analytics package

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { HoverProvider } from './contexts/Cursor.context'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Analytics />
     <BrowserRouter>
       <HoverProvider>
         <App />
